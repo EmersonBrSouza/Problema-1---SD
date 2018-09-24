773327463542d27284996da05b98bcaff2c7ba5e
EmersonBrSouza <emerson_souza_fsa@hotmail.com>
2018-09-24 10:38:06 -0300

    Software started

---
+++ software/lcd_controller/main.c
@@ -0,0 +1,44 @@
+/*
+ * main.c
+ *
+ *  MI - Sistemas Digitais
+ *      Author: Diego Lourenço, Emerson Souza, Lokisley Oliveira
+ */
+
+#include <system.h>;
+#include <io.h>;
+
+#define START_4_BITS 0x28
+#define START_8_BITS 0x38
+
+#define CLEAR_DISPLAY 0x01
+#define RETURN_HOME 0x02
+
+#define CURSOR_OFF 0x0C
+#define CURSOR_BLOCK 0x0D
+#define CURSOR_BAR 0x0E;
+#define CURSOR_BLINK 0x0F
+
+#define MOVE_CURSOR_TO_LEFT 0x10
+#define MOVE_CURSOR_TO_RIGHT 0x14
+
+#define MOVE_MESSAGE_TO_LEFT 0x18
+#define MOVE_MESSAGE_TO_RIGHT 0x1C
+
+void startDisplay (int num_bits);
+void sleep (int milliseconds);
+
+int main () {
+	startDisplay(START_8_BITS);
+}
+
+void startDisplay (int num_bits) {
+	IOWR(0,0,num_bits);
+}
+
+void sleep (int milliseconds) {
+	int clock = milliseconds * 1000;
+	int i = 0;
+
+	while (i < clock) { i++; }
+}
---
+++ software/lcd_controller/obj/default/main.d
@@ -0,0 +1,10 @@
+obj/default/main.o: main.c ../lcdlib_bsp/system.h ../lcdlib_bsp/linker.h \
+ ../lcdlib_bsp//HAL/inc/io.h ../lcdlib_bsp//HAL/inc/alt_types.h
+
+../lcdlib_bsp/system.h:
+
+../lcdlib_bsp/linker.h:
+
+../lcdlib_bsp//HAL/inc/io.h:
+
+../lcdlib_bsp//HAL/inc/alt_types.h:
